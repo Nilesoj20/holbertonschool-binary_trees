522a9242104de6c8c94295ed69a901fdca6062d6
“Oscarsa3” <oscarorlandosalinassilvestre@gmail.com>
2023-05-17 17:14:07 -0700

    fix: errors of max height

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -10,9 +10,11 @@ int binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
 	if (tree->left == NULL || tree->right == NULL)
 		return (0);
-	if (derecha(tree->right) == izquierda(tree->left))
+	if ((derecha(tree->right) + izquierda(tree->left)) % 2 == 0)
 		return (1);
 	return (0);
 }
