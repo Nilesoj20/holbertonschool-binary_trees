278941ea1c7df41175b71608b7339eadc0649419
“Oscarsa3” <oscarorlandosalinassilvestre@gmail.com>
2023-05-17 17:52:46 -0700

    fix: error with child of root node (no grand parent)

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -8,9 +8,7 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 	int tio, pa;
 
-	if (node == NULL)
-		return (NULL);
-	if (node->parent->parent->left == NULL || node->parent->parent->right == NULL)
+	if (node == NULL || node->parent == NULL)
 		return (NULL);
 	if (node->parent->parent == NULL)
 		return (NULL);
